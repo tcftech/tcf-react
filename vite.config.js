4d98e7c430940768c2e0d9716c1dfc674e66549b
tcftech <tcftech11@gmail.com>
2024-08-29 23:56:36 +0530

    Update vite.config.js
    update vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
+    host: '0.0.0.0', // Listen on all network interfaces
+  },
+});
+
